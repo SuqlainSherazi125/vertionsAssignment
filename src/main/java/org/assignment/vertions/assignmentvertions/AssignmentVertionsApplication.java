9ab9d9220f6948497901825ced0978bc812a6fa8
SuqlainSherazi125 <suqlain.sherazi786@gmail.com>
2024-03-26 19:41:29 +0500

    Pushing repo on git

---
+++ src/main/java/org/assignment/vertions/assignmentvertions/AssignmentVertionsApplication.java
@@ -0,0 +1,13 @@
+package org.assignment.vertions.assignmentvertions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentVertionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentVertionsApplication.class, args);
+	}
+
+}
